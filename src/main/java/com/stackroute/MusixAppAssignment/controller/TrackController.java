c35b3045883ffe49f7c2d42f2dc443b0993f1431
velsuripoornima <51950104+velsuripoornima@users.noreply.github.com>
2019-08-02 09:46:29 +0530

    Update TrackController.java

--- src/main/java/com/stackroute/MusixAppAssignment/controller/TrackController.java
+++ src/main/java/com/stackroute/MusixAppAssignment/controller/TrackController.java
@@ -24,6 +24,7 @@ import java.util.List;
 
 import static org.springframework.data.repository.init.ResourceReader.Type.JSON;
 
+//we are configuring the rest controller
 @RestController
 public class TrackController {
 
@@ -64,6 +65,8 @@ public class TrackController {
 
     }
 
+    
+    //getting track by id
     @PutMapping("/track/{id}")
     public ResponseEntity<?> getTrack(@RequestBody Track track, @PathVariable("id") int id) throws Exception {
 
@@ -79,6 +82,7 @@ public class TrackController {
 
     }
 
+    //delete track by id
     @DeleteMapping("/track/{id}")
     public ResponseEntity<?> deleteTrack(@PathVariable int id) {
         trackService.deleteTrack(id);
@@ -86,30 +90,37 @@ public class TrackController {
     }
 
 
+    //getting track by name
     @GetMapping("/track/{name}")
     public ResponseEntity<?> trackByName(@PathVariable String name) {
 
         return new ResponseEntity<>(trackService.trackByName(name), HttpStatus.OK);
     }
 
+    //search by id and name
     @GetMapping("/track/{id}/{name}")
     public ResponseEntity<?> searchByName(@PathVariable int id, @PathVariable String name) {
         return new ResponseEntity<>(trackService.searchByNameAndId(id, name), HttpStatus.OK);
 
     }
 
+    //getting list of tack by last.fm 
     @GetMapping("listOfTrack")
     public ResponseEntity<?> getListOfTrackFromLastFm(@RequestParam String url) throws Exception {
 
-
+        //getting all tracks in json formate
         RestTemplate restTemplate = new RestTemplate();
         String string = restTemplate.getForObject(url,String.class);
         System.out.println(string);
+        
+        //converting the json object to java object
         ObjectMapper objectMapper = new ObjectMapper();
         Root root = objectMapper.readValue(string, Root.class);
         List<Track> trackList = root.getResults().getTrackmatches().getTrack();
         System.out.println(trackList);
         List<Track> savedTrackList = new ArrayList<>();
+        
+        //getting list of tracks by using for loop
         for (Track user:trackList) {
             Track user1 = trackService.saveTrack(user);
             savedTrackList.add(user1);
